79aed3b94e63b3701b2cc446aed74df74d6b262d
Claire Watson <watson.e.claire@gmail.com>
2018-01-31 19:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ function prependKitten(name){
   return [name, ...kittens]
 }
 
-function removeLastKitten(name){
+function removeLastKitten(){
   kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(){
+  kittens.slice(1)
 }
